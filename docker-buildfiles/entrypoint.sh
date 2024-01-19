8ab9a71863c1a0961850385953ce6d3f3a3d9325
Ben Franske <ben.mm@franske.com>
2024-01-19 17:24:15 -0600

    update to use docker volumes for persistant data

--- docker-buildfiles/entrypoint.sh
+++ docker-buildfiles/entrypoint.sh
@@ -2,7 +2,24 @@
 
 ## entry point file, needed to be able to pass ENV vars from docker-compose.yml to the containers.
 
-touch /entrypointhasrunonce
+#Things to do only the first time this entrypoint runs
 
+if [ ! -f /mautic-docker-data/entrypointrunonce ]; then
+    # These things are only ever done once as the /mautic-docker-data/ directory persists on a docker volume
+
+    # Install Mautic via composer
+    chown apache:apache /var/www
+    chown apache:apache /var/www/mautic
+    cd /var/www/mautic
+    runuser -u apache -- composer create-project mautic/recommended-project:^5 /var/www/mautic --no-interaction
+
+    # Install Amazon Mailer
+    cd /var/www/mautic
+    runuser -u apache -- composer require symfony/amazon-mailer 
+
+    touch /mautic-docker-data/entrypointrunonce
+fi
+
+# Things to do everytime the container is launched
 crond
 /usr/sbin/httpd -D FOREGROUND
